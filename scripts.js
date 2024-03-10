e1d14614bee7757c53c85818ac4579335f113803
JORGEMELO21 <142284641+JORGEMELO21@users.noreply.github.com>
2024-03-10 16:47:41 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+
+
+const key "894d9cff75f7fd4877c4636f54d203dd" // strig para identificação da API de previsão do tempo//
+
+
+function cliqueiNoBotao() {
+    const input = document.querySelector(".inputCidade") .value
+    
+    console.log(input)
+}
